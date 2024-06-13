deb15a3020149688c0419a8cc7f66413d65f792d
Hirwa Arnold <arnold.hirw@gmail.com>
2024-06-14 01:30:53 +0200

    edit

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,17 +4,17 @@
 
 def minOperations(n: int) -> int:
     """ Minimum Operations needed to get n H characters """
-    if not isinstance(h, int) or h <= 0:
-        return 0
-    copy_var = 1
-    rep_var = 1
-    op_count = 0
-    while rep_var < h:
-        if h % rep_var == 0:
-            copy_var = rep_var
-            rep_var *= 2
-            op_count += 2
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
         else:
-            rep_var += copy_var
-            op_count += 1
-    return op_count
+            op += 1
+            body += next
+    if len(body) != n:
+        return 0
+    return op
